0871f159bb03e4fe6ce45726aacc06918dc9e60d
Omar Albacha <oalbacha@gmail.com>
2022-05-24 14:09:18 +0400

    fixed some styles

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,30 +5,26 @@ import './Counter.css';
 export default function Counter() {
 
 	return (
-		<div className="card">
-			<div className="header">
-				<div className="social">
-					<a className="icon-container" href="https://api.whatsapp.com/send?phone=+51900617146" title="WhatsApp" target="_blank" rel="noopener noreferrer">
-						<FaWhatsapp className="icon" />
-					</a>
-					<a className="icon-container" href="https://www.linkedin.com/in/nisha-mistry-96371b13b/" title="LinkedIn" target="_blank" rel="noopener noreferrer">
-						<FaLinkedinIn className="icon" />
-					</a>
-				</div>
-			</div>
+		<div className="page">
+			<header className="header">
+				<a className="icon-container" href="https://api.whatsapp.com/send?phone=+51900617146" title="WhatsApp" target="_blank" rel="noopener noreferrer">
+					<FaWhatsapp className="icon" />
+				</a>
+				<a className="icon-container" href="https://www.linkedin.com/in/nisha-mistry-96371b13b/" title="LinkedIn" target="_blank" rel="noopener noreferrer">
+					<FaLinkedinIn className="icon" />
+				</a>
+			</header>
 
-			<div className="content">
-				<div className="title-holder">
-					<div className="logo">
-						<img width='250' height='250' src="./images/ttp-square-logo.svg" alt="the therpay place uk logo" />
-					</div>
+			<main className="content">
+				<img className="logo" src="./images/ttp-square-logo.svg" alt="the therapy place uk logo" />
+				<div className='copy'>
 					<h1>The Therapy Place</h1>
 					<p>Website coming soon. Please check back soon or email us to book a session today.</p>
 				</div>
 				<a className="cta" href="mailto:nisha@thetherapyplaceuk.com">
 					Send us an email
 				</a>
-			</div>
+			</main>
 		</div>
 	);
 }
